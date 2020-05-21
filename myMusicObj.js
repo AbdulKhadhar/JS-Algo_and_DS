d1875f6b80062ecfb409f49e43eeea77a67e995a
Abdul Khadar <42581864+AbdulKhadhar@users.noreply.github.com>
2020-05-21 18:40:17 +0530

    Manipulating Complex Objects

---
+++ myMusicObj.js
@@ -0,0 +1,21 @@
+var myMusic = [
+  {
+    "artist": "Billy Joel",
+    "title": "Piano Man",
+    "release_year": 1973,
+    "formats": [
+      "CD",
+      "8T",
+      "LP"
+    ],
+    "gold": true
+  },{
+    
+      "artist":"Ed Sheeran",
+      "title":"Shape of You",
+      "release_year":2016,
+      "formats":["MP3","MP4"]
+    
+  }
+  // Add a record here
+];
